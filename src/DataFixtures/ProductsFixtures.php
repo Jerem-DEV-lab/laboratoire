b0f25dacba0d642845b1d27ef79b08f84819fb4b
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2020-06-21 14:24:26 +0200

    feat: Fixtures products datas

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -2,20 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Products;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-use Faker/Faker;
+use Faker\Factory;
 class ProductsFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         //Create 20 products Fixtures
-        $faker = Faker\Factory::create();
-
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 10; $i++)
+        {
+            $product = new Products();
+            $product
+                ->setName($faker->words(3, true))
+                ->setDescription($faker->sentences(3, true))
+                ->setPrice($faker->numberBetween(10, 50))
+           ;
+            $manager->persist($product);
+        }
         $manager->flush();
     }
 }
